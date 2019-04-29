ce8428882ea177a191b86806c094f63065fef775
Samsul Arifin <arifinsam.sam@gmail.com>
2019-04-29 22:44:47 +0700

    add migration file

---
+++ database/migrations/2019_04_29_152911_create_table_indicator_value.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableIndicatorValue extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('indicator_value', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('indicator_id');
+            $table->integer('value');
+            $table->integer('report_id');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('indicator_value');
+    }
+}
---
+++ database/migrations/2019_04_29_153130_create_table_report.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableReport extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('report_template_id');
+            $table->dateTime('report_date');
+            $table->integer('author_id');
+            $table->integer('status');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('report');
+    }
+}
