b27170c135bfba52e588b97676e401ba10b94584
andy-shi88 <shi88.andy@gmail.com>
2019-05-01 18:51:19 +0800

    initialize service layer

---
+++ app/Http/Controllers/ReportTemplateController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+use App\Http\Builders\ResponseBuilder;
+use App\Http\Models\ReportTemplate;
+use App\Http\Services\ReportTemplateService;
+
+class ReportTemplateController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $model = new ReportTemplate();
+        parent::__construct(new ReportTemplateService($model));
+    }
+
+    public function get(Request $request) {
+      $responseBuilder = new ResponseBuilder();
+      $page = $request->input('page') ? : 1;
+      $limit = $request->input('limit') ? : 10;
+      $reportTemplates = $this->service->get($page, $limit);
+      $response = $responseBuilder->setData($reportTemplates['data'])->setMessage('fetched report template')
+        ->setTotal($reportTemplates['total'])->setCount($limit)->setPage($page)
+        ->setSuccess(true)->build();
+      return $response;
+    }
+
+    public function find($id) {
+      $responseBuilder = new ResponseBuilder();
+      $reportTemplate = $this->service->find($id);
+      if ($reportTemplate) {
+        $response = $responseBuilder->setData($reportTemplate)->setMessage('fetched report template')->setSuccess(true)->build();
+        return $response;
+      } else {
+        $response = $responseBuilder->setData($reportTemplate)
+            ->setMessage('report template with id: ' . $id . ' not found')
+            ->setSuccess(false)->setStatus(404)->build();
+        return $response;
+      }
+    }
+
+    public function create(Request $request) {
+      $responseBuilder = new ResponseBuilder();
+      try {
+        $this->validate($request, [
+          'name' => 'required',
+          'label' => 'required'
+        ]);
+        $data = new ReportTemplate();
+        $data->name = $request->input('name');
+        $data->label = $request->input('label');
+        $data->author_id = "andy-shi88"; // TODO: get auth from middleware
+        $reportTemplate = $this->service->save($data);
+        $response = $responseBuilder->setData($reportTemplate)->setMessage('report template created successfully')
+          ->setSuccess(true)->build();
+        return $response;
+      } catch (ValidationException $e) {
+        $response = $responseBuilder->setMessage("invalid payload provided")
+          ->setSuccess(false)->setStatus(422)->build();
+        return $response;
+      } catch (\Exception $e) {
+        $response = $responseBuilder->setMessage("unknown error occured: contact your administrator")
+          ->setSuccess(false)->setStatus(500)->build();
+        return $response;
+      }
+    }
+
+    public function update(Request $request, $id) {
+      $responseBuilder = new ResponseBuilder();
+      try {
+        $this->validate($request, [
+          'name' => 'required',
+          'label' => 'required',
+        ]);
+        $data = $this->service->find($id);
+        if ($data) {
+          $data->name = $request->input('name');
+          $data->label = $request->input('label');
+          $data->author_id = "andy-shi88"; // TODO: get auth from middleware
+          $reportTemplate = $this->service->save($data);
+          $response = $responseBuilder->setData($reportTemplate)->setMessage('report template updated successfully')
+            ->setSuccess(true)->build();
+          return $response;
+        } else {
+          $response = $responseBuilder->setData($reportTemplate)
+            ->setMessage('report template with id' . $id . ' not found')
+            ->setSuccess(false)->setStatus(404)->build();
+          return $response;
+        }
+      } catch (ValidationException $e) {
+        $response = $responseBuilder->setMessage("invalid payload provided")
+          ->setSuccess(false)->setStatus(422)->build();
+        return $response;
+      } catch (\Exception $e) {
+        $response = $responseBuilder->setMessage("unknown error occured: contact your administrator")
+          ->setSuccess(false)->setStatus(500)->build();
+        return $response;
+      }
+    }
+
+    public function delete($id) {
+      $responseBuilder = new ResponseBuilder();
+      try {
+        $result = $this->service->delete($id);
+        if ($result) {
+          $response = $responseBuilder->setMessage('report template deleted successfully')
+            ->setSuccess(true)->build();
+          return $response;
+        } else {
+          $response = $responseBuilder->setData($reportTemplate)
+            ->setMessage('report template with id: ' . $id . ' not found')
+            ->setSuccess(false)->setStatus(404)->build();
+          return $response;
+        }
+      } catch (\Exception $e) {
+        $response = $responseBuilder->setMessage("unknown error occured: contact your administrator")
+          ->setSuccess(false)->setStatus(500)->build();
+        return $response;
+      }
+    }
+}
---
+++ app/Http/Services/BaseService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Services;
+
+/**
+ * BaseService class that provide basic CRUD functionality.
+ */
+class BaseService {
+
+  protected $model = null;
+
+  function __construct($model) {
+    $this->model = $model;
+  }
+
+  /**
+   * get get all row paginated
+   * @param $page Integer page number
+   * @param $limit Integer maximum number of item to return
+   * @param $condition [Array] conditional
+   * @param $sort [Array['key', 'value']] sort by. value being either `ASC` or `DESC`
+   * @return ['total', 'data']. total: total of row in the database
+   */
+  function get($page = 1, $limit = 10, $condition=[], $sort=['created_at', 'DESC']) {
+    $total = $this->model::count();
+    $data = $this->model::where($condition)->limit($limit)->offset(($page-1)*$limit)->orderBy($sort[0], $sort[1])->get()->toArray();
+    return [
+      'total' => $total,
+      'data' => $data,
+    ];
+  }
+
+  /**
+   * find by id, default implementation of finding single row
+   * @param $id Mix any value of the table id
+   * @return ['data']
+   */
+  function find($id) {
+    return $this->model::where(['id' => $id])->first();
+  }
+
+  /**
+   * save or update record
+   * @param $payload Mix
+   * @return Array
+   */
+  function save($payload) {
+    $payload->save();
+    return $payload;
+  }
+
+  /**
+   * delete row
+   * @param $id model id
+   */
+  function delete($id) {
+    $data = $this->find($id);
+    if ($data) {
+      $data->delete();
+      return true;
+    }
+    return false;
+  }
+
+}
