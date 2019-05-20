3a281c36e7e7793032d0143042742728c58ba184
Samsul Arifin <arifinsam.sam@gmail.com>
2019-05-20 12:40:51 +0700

    fix report service

--- app/Http/Services/ReportService.php
+++ app/Http/Services/ReportService.php
@@ -7,23 +7,8 @@ namespace App\Http\Services;
  */
 class ReportService extends BaseService
 {
-
-    protected $model;
-
     function __construct($model)
     {
-        $this->model = $model;
         parent::__construct($model);
-    }
-
-    public function get($page = 1, $limit = 10, $condition = [], $sort = ['created_at', 'DESC'])
-    {
-        $reports = $this->model->where($condition)->limit($limit)->offset(($page - 1) * $limit)->orderBy($sort[0], $sort[1])->get();
-        $total = $this->model::count();
-
-        return [
-            'total' => $total,
-            'data' => $reports,
-        ];
     }
 }
