ef00971e8ce5a324b03662f86ab497399885a3af
andy-shi88 <shi88.andy@gmail.com>
2019-06-17 19:38:49 +0800

    override find and get

--- app/Http/Services/ReportTemplateService.php
+++ app/Http/Services/ReportTemplateService.php
@@ -11,4 +11,20 @@ class ReportTemplateService extends BaseService {
     parent::__construct($model);
   }
 
+  function get($page = 1, $limit = 10, $condition=[], $sort=['created_at', 'DESC']) {
+    $total = $this->model::count();
+    $data = $this->model::where($condition)->limit($limit)->offset(($page-1)*$limit)
+      ->orderBy($sort[0], $sort[1])
+      ->with(['indicators'])
+      ->get()->toArray();
+    return [
+      'total' => $total,
+      'data' => $data,
+    ];
+  }
+
+
+  function find($id) {
+    return $this->model::where(['id' => $id])->with(['indicators'])->first();
+  }
 }
