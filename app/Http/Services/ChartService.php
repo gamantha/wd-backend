7e6b3bdce6965c4bb245ab67d2e606606b98c154
andy-shi88 <shi88.andy@gmail.com>
2019-08-11 11:53:54 +0800

    group indicator value by month

--- app/Http/Services/ChartService.php
+++ app/Http/Services/ChartService.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services;
 
 use DB;
+use Carbon\Carbon;
 use App\Http\Models\Chart;
 use App\Http\Models\ChartIndicator;
 
@@ -49,10 +50,28 @@ class ChartService extends BaseService {
 
   function get($page = 1, $limit = 10, $condition=[], $sort=['created_at', 'DESC']) {
     $total = $this->model::count();
-    $data = $this->model::with('indicatorValue.indicator')->where($condition)->limit($limit)->offset(($page-1)*$limit)->orderBy($sort[0], $sort[1])->get()->toArray();
+    $data = $this->model::with('indicatorValue.indicator')
+        ->where($condition)
+        ->limit($limit)
+        ->offset(($page-1)*$limit)
+        ->orderBy($sort[0], $sort[1])
+        ->get();
+    $result = [];
+    foreach($data as $chart) {
+        $chartIndicatorsCollection = collect($chart->indicatorValue);
+        $chartIndicators = $chartIndicatorsCollection->whereBetween('created_at', [$chart->from, $chart->to]);
+        $groupedIndicators = $chartIndicators->sortBy('created_at')->groupBy(
+            function($d) {
+                return Carbon::parse($d->created_at)->format('m');
+            }
+        );
+        $chart->indicators = $groupedIndicators;
+        unset($chart->indicatorValue);
+        array_push($result, $chart);
+    }
     return [
       'total' => $total,
-      'data' => $data,
+      'data' => $result,
     ];
   }
 }
