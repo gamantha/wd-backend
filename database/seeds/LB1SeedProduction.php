cfef0c898053cba6e456367e5b7d15563109b4d1
andy-shi88 <shi88.andy@gmail.com>
2019-08-21 20:41:23 +0800

    update 2 seed

--- database/seeds/LB1SeedProduction.php
+++ database/seeds/LB1SeedProduction.php
@@ -284,6 +284,136 @@ class LB1SeedProduction extends Seeder
                 'malaria_tanpa_pemeriksaan_lab' => 'Malaria tanpa pemeriksaan lab / malaria klinis (Clinical malaria)',
                 'anthrax' => 'Anthrax',
             ],
+            [ // parent 6 childs
+                'infeksi_gonokok' => 'Infeksi Gonokok (Gonorrhea infection)',
+                'non_gonokok' => 'Non Gonokok (Non Gonorrhea infection)',
+                'penyakit_kelamin_lain' => 'Penyakit kelamin lain (other STI)'
+            ],
+            [ // parent 7 childs
+                'frambusia' => 'Frambusia (Yows)',
+                'filariasis' => 'Filariasis',
+                'penyakit_cacingan' => 'Penyakit cacingan (Worm disease)',
+                'scabies' => 'Scabies',
+            ],
+            [ // parent 8 childs
+                'gangguan_psikotik' => 'Gangguan psikotik (Physotic disorder)',
+                'gangguan_neurotik' => 'Gangguan neurotik (Neurotik disorder)',
+                'retardasi_mental' => 'Retardasi mental (Mental retardation)',
+                'gangguan_kesehatan_jiwa_bermula_pada_bayi_anak_remaja_perkembangan' => 'Gannguan kesehatan jiwa bermula pada bayi, anak , remaja dan perkembangan (early onset mental disorder)',
+                'penyakit_jiwa_lainnya' => 'Penyakit jiwa lainnya (other mental disorder)'
+            ],
+            [ // parent 9 childs
+                'epilepsi' => 'Epilepsi',
+                'penyakit_dan_kelainan_susunan_saraf_lainnya' => 'Penyakit dan kelainan susunan saraf lainnya (other neurological disease)',
+            ],
+            [ // parent 10 childs
+                'glaukoma' => 'Glaukoma',
+                'katarak' => 'Katarak (Catarract)',
+                'kelainan_refraksi' => 'Kelainan refraksi (Refractive disorder)',
+                'kelainan_kornea' => 'Kelainan kornea (Corneal disorder)',
+                'penyakit_mata_lainnya' => 'Penyakit mata lainnya (other eyes disease/disorder)',
+            ],
+            [ // parent 11 childs
+                'infeksi_telinga_tengah' => 'Infeksi telinga tengah (Otitis media )',
+                'infeksi_mastoid' => 'Infeksi mastoid (Mastoid infection)',
+            ],
+            [ // parent 12 childs
+
+            ],
+            [ // parent 13 childs
+                'tonsilitis' => 'Tonsilitis',
+                'infeksi_akut_lain_pada_saluran_nafas_atas' => 'Infeksi akut lain pada saluran nafas atas (other infection in upper respiratory)',
+                'penyakit_lain_pada_saluran_nafas_bawah' => 'PENYAKIT LAIN PADA SALURAN PERNAFASAN BAWAH (Lower respiratory tract infection)',
+            ],
+            [ // parent 14 childs
+                'bronkitis' => 'Bronkitis',
+                'asma' => 'Asma',
+                'penyakit_lain_dari_saluran_nafas_bawah' => 'Penyakit lain dari saluran nafas bawah (other disease from lower respiratory)',
+            ],
+            [ // parent 15 childs
+                'karies_gigi' => 'Karies gigi (tooth caries)',
+                'penyakit_pulpa_dan_jaringan_periapikal' => 'Penyakit pulpa dan jaringan periapikal (Pulp and periapical disease)',
+                'gingivitis_dan_penyakit_periodontal' => 'Gingivitis dan penyakit periodontal (Gingivitis & periodontal disease)',
+                'gangguan_gigi_dan_jaringan_penyangga_lain' => 'Gangguan gigi dan jaringan penyangga lain (dental an other supporting tissue disorder)',
+                'penyakit_rongga_mulut_kelenjar_ludah_rahang_dan_lainnya' => 'Penyakit rongga mulut, kelenjar ludah, rahang dan lainnya (Mouth, gland saliva, jaws and other disease)',
+            ],
+            [ // parent 16 childs
+
+            ],
+            [ // parent 17 childs
+                'keguguran' => 'Keguguran (Miscarriage)',
+                'pendarahan_pada_kehamilan_persalinan_dan_masa_nifas' => 'perdarahan pada kehamilan, persalinan dan masa nifas (bleeding during pregnancy, delivery and post partum)',
+                'keracunan_kehamilan' =>  'Keracunan kehamilan (Eklampsia)',
+                'partus_lama' => 'Partus lama (Prolonged labour)',
+                'infeksi_pada_masa_kehamilan_persalinan_dan_masa_nifas' => 'Infeksi pada masa kehamilan, persalinan dan nifas (Infection during pregancy, delivery and post partum)',
+                'hyperemesis' => 'Hyperemesis',
+            ],
+            [ // parent 18 childs
+                'trauma_lahir' => 'Trauma lahir (birth trauma)',
+                'asfiksia' => 'Asfiksia (Asphyxia)',
+                'tetanus_neonatarum' => 'Tetanus neonatorum',
+            ],
+            [ // parent 19 childs
+                'kecelakaan_dan_ruda_paksa' => 'Kecelakaan dan ruda paksa (Accident & trauma)',
+                'keracunan_bahan_kimia' => 'Keracunan bahan kimia (Chemical intoxication)',
+                'keracunan_makanan' => 'Keracunan makanan (food intoxication)',
+            ],
+            [ // parent 20 childs
+                'penyakit_kulit_infeksi' => 'Penyakit kulit infeksi (skin infection)',
+                'penyakit_kulit_alergi' => 'Penyakit kulit alergi (Skin allergic)',
+                'penyakit_karena_jamur' => 'Penyakit karena jamur (Skin infection ec fungal)',
+            ],
+            [ // parent 21 childs
+
+            ],
+            [ // parent 22 childs
+                'infeksi_pada_umbilikus' => 'Infeksi pada umbilikus (umbilical cord infection)',
+                'alergi_makanan' => 'alergi makanan (food allergy)',
+                'fixed_drug_eruption' => 'fixed drug eruption (FDE)',
+                'syok' => 'Syok (Shock)',
+                'anemia' => 'Anemia',
+                'sida' => 'SIDA (AIDS)',
+                'limfadenitis' => 'Limfadenitis',
+                'refluks_gastroesofageal' => 'Refluks gastroesofageal (GERD) (Gastroesofageal reflux disease)',
+                'suspect_gastritis' => 'Suspect gastritis/dispepsia (Dyspepsia)',
+                'intoleransi_makanan' => 'Intoleransi makanan (Food intolerance)',
+                'demam_tifoid' => 'Demam Tifoid (Thypoid fever)',
+                'pendarahan_saluran_makan_bagian_atas' => 'Perdarahan saluran makan bagian atas (upper digestive tract bleeding)',
+                'perdarahan_saluran_makan_bagian_bawah' => 'Perdarahan saluran makan bagian bawah (Lower digestive tract bleeding) ',
+                'hordeolum' => 'Hordeolum',
+                'konjungtivitis' => 'Konjungtivitis',
+                'blefaritis' => 'Blefaritis',
+                'perdarahan_subkonjungtiva' => 'Perdarahan subkonjungtiva (Sub conjungtival bleeding)',
+                'benda_asing_di_konjungtiva' => 'Benda asing di konjungtiva (foreign body in conjungtiva)',
+                'otitis_externa' => 'Otitis externa (External ear infection)',
+                'seruman_prop' => 'Serumen prop (Earwax blockage)',
+                'benda_asing_di_hidung' => 'Benda asing di hidung (Foreign body in nose)',
+                'angina_pektoris' => 'Angina pektoris (Chest pain)',
+                'infark_miokard' => 'Infark miokard (Myocardial infarction)',
+                'aritmia_cordis' => 'Aritmia cordis (iregular heart rhytm)',
+                'gagal_jantung' => 'Gagal jantung (heart failure)',
+                'cardiorespiratory_arrest' => 'Cardiorespiratory arrest',
+                'stroke_infark_serebral' => 'Stroke infark serebral',
+                'lipoma_cyst_ateroma_ganglioma' => 'Lipoma, cyst, Ateroma, Ganglioma',
+                'kejang_demam' => 'Kejang demam (Febril seizure)',
+                'vertigo' => 'Vertigo',
+                'delirium' => 'Delirium',
+                'migrain' => 'Migrain',
+                'bells_palsy' => 'Bells Palsy',
+                'tension_headache' => 'Tension headache',
+                'dementia' => 'Dementia',
+                'epistaksis' => 'Epistaksis (Bleeding from nose)',
+                'furunkel' => 'Furunkel (boils)',
+                'luka_bakar_derajat_1_dan_2' => 'Luka bakar derajat 1 dan 2 (Burn wound first and second degree)',
+                'diabetes_mellitus' => 'Diabetes mellitus',
+                'malnutrisi_energi_protein' => 'malnutrisi energi protein (Malnutrition)',
+                'hiperurisemia' => 'Hiperurisemia/Gout artrhitis',
+                'dislipidemia' => 'Dislipidemia/hipercholesterolemia',
+                'kehamilan_normal' => 'Kehamilan normal (normal pregnancy',
+                'pre_eclampsia' => 'pre eclampsia',
+                'ketuban_pecah_dini' => 'Ketuban pecah dini (Premature rupture of membrane)',
+                'mastitis' => 'Mastitis',
+            ]
         ];
         foreach ($categories as $category) {
             $c = [
